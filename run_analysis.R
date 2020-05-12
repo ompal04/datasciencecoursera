3d8330334ad66e617815e5e3b71cffe8f3d24aae
ompal04 <64707514+ompal04@users.noreply.github.com>
2020-05-12 15:52:10 +0530

    Update run_analysis.R
    Added comments

--- run_analysis.R
+++ run_analysis.R
@@ -1,5 +1,16 @@
+## Uses deplyr package to tidy up smartphones data
+
+## Downloads the data set into datasciencecoursera folder on the local desktop (existing files are overwritten by default)
+download.file("https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip", filename, method="curl")
+
+## Checks whether the file had been unzipped before
+if (!file.exists("UCI HAR Dataset")) { 
+  unzip(filename) 
+}
+
 library(dplyr)
 
+## Reads data from all the text files in the UCI HAR folder into different dataframes
 features <- read.table("./UCI HAR Dataset/features.txt", col.names = c("n","functions"))
 activities <- read.table("./UCI HAR Dataset/activity_labels.txt", col.names = c("code", "activity"))
 subject_test <- read.table("./UCI HAR Dataset/test/subject_test.txt", col.names = "subject")
@@ -9,6 +20,7 @@ subject_train <- read.table("./UCI HAR Dataset/train/subject_train.txt", col.nam
 x_train <- read.table("./UCI HAR Dataset/train/X_train.txt", col.names = features$functions)
 y_train <- read.table("./UCI HAR Dataset/train/y_train.txt", col.names = "code")
 
+## Dataframes containing test and train data are merged, mean and std dev are extracted into TidyData dataframe
 X <- rbind(x_train, x_test)
 Y <- rbind(y_train, y_test)
 Subject <- rbind(subject_train, subject_test)
@@ -16,6 +28,7 @@ Merged_Data <- cbind(Subject, Y, X)
 
 TidyData <- Merged_Data %>% select(subject, code, contains("mean"), contains("std"))
 
+## Activities are named in the TidyData dataset and the dataset is labelled
 TidyData$code <- activities[TidyData$code, 2]
 
 names(TidyData)[2] = "activity"
@@ -32,11 +45,8 @@ names(TidyData)<-gsub("-freq()", "Frequency", names(TidyData), ignore.case = TRU
 names(TidyData)<-gsub("angle", "Angle", names(TidyData))
 names(TidyData)<-gsub("gravity", "Gravity", names(TidyData))
 
+##Extracts averages into FinalData and extracts it into a txt file
 FinalData <- TidyData %>%
   group_by(subject, activity) %>%
   summarise_all(funs(mean))
 write.table(FinalData, "FinalData.txt", row.name=FALSE)
-
-str(FinalData)
-
-FinalData
